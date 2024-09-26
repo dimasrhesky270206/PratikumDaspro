78bb8df055f65333b3a29fa86f8dbe2ede3081c0
dimasrhesky270206 <dimashandaresky@gmail.com>
2024-09-26 08:45:20 +0700

    pemilihan bilangan dimas

--- Pratikum05/PemilihanBilangan06.java
+++ Pratikum05/PemilihanBilangan06.java
@@ -2,25 +2,22 @@ import java.util.Scanner;
 /**
  * PemilihanBilangan06
  */
-public class PemilihanBilangan06{
+public class PemilihanBilangan06 {
 
-public static void main(String[]args) {
-    // Deklarasi scanner
-    Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        // Deklarasi scanner
+        Scanner sc = new Scanner(System.in);
 
-    System.out.print("Masukan sebuah angka: ");
-    int angka = sc.nextInt();
-    
-    if (angka % 2 ==0)
-    {
-        System.out.println("Angka"+ angka +" termasuk bilangan genap");
+        System.out.print("Masukan sebuah angka: ");
+        int angka = sc.nextInt();
+
+        // Memanfaatkan ternary operator untuk menentukan genap atau ganjil
+        String hasil = (angka % 2 == 0) ? "Angka " + angka + " termasuk bilangan genap" : "Angka " + angka + " termasuk bilangan ganjil";
+        
+        // Menampilkan hasil
+        System.out.println(hasil);
+
+        // Menutup scanner
+        sc.close();
     }
-    else
-    {
-        System.out.println("Angka " + angka +"termasuk bilangan ganjil");
-    }
-
-    // Menutup scanner
-    sc.close();
-}
 }
